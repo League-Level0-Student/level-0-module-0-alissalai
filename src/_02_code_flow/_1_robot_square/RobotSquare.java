df472d2ee5b2384c51090854d8a6898d0bceabcd
root <root@0a76b52db9aa>
2020-06-11 23:59:30 +0000

    Robot fun

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -13,26 +13,29 @@ public class RobotSquare {
     public static void main(String[] args){
  
     	// This code makes a new Robot
-    	Robot rob = new Robot();
+    	Robot madi = new Robot();
 
         // Put the robot's pen down so it can draw
 		//        Use the     rob.penDown();   command.
-
+madi.sparkle();
+madi.penDown();
         // SPEED. Make the robot move quickly.
 		//        Use the     rob.setSpeed();   command.
-
+madi.setSpeed(20);
 
         // DRAW.  Draw a line
     	//		Use the     rob.move(); command
-
+for(int i = 0; i<4;i++){
+madi.move(250);
 
 		// TURN.  Turn 90 degrees
     	//      Use the     rob.turn();  command
-
-
+madi.turn(90);
+}
         // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
 		//        Did your Robot draw a square?
 
+}
 
     }
-}
+
--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -5,31 +5,35 @@ package _02_code_flow._2_robot_spiral;
  */
 
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class RobotSpiral {
 
 	public static void main(String[] args) {
-		
-		// Create a new Robot
-		
-		// Set your robot's pen down 
-		
-		// SPEED. Set the robot to go at max speed (100)
-		
-        // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
-		
-				// Change the robot pen color to random
-			
-				// DRAW.    Move the robot (5*i) pixels
-				//          "i" is the variable in the for loop	
-			
-				// TURN.    Turn the robot (360/7) degrees to the right
-			
-				// Change the robot pen width to (i)
 
+		// Create a new Robot
+		Robot leila = new Robot();
+		// Set your robot's pen down
+		leila.sparkle();
+		leila.penDown();
+		// SPEED. Set the robot to go at max speed (100)
+		leila.setSpeed(50);
+        // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
+		for(int i = 0;i<75;i++) {
+				// Change the robot pen color to random
+		//leila.setPenColor(Color.BLUE);
+		leila.setRandomPenColor();	
+			// DRAW.    Move the robot (5*i) pixels
+				//          "i" is the variable in the for loop	
+		leila.move(5*i);
+				// TURN.    Turn the robot (360/7) degrees to the right
+		leila.turn(360/3);
+				// Change the robot pen width to (i)
+		leila.setPenWidth(i);
 		
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
-		
+		}
 	}
 }
